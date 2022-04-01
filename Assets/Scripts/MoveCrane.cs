17520ff415f2e3182d107cd9b12142538538882a
Joshan Rai <pradheon@gmail.com>
2022-04-01 12:59:27 -0700

    Crane Movement Added
    - Moves Up & Down
    - Attaches GameObject Sphere to Crane

--- Assets/Scripts/MoveCrane.cs
+++ Assets/Scripts/MoveCrane.cs
@@ -4,36 +4,74 @@ using UnityEngine;
 
 public class MoveCrane : MonoBehaviour
 {
-    public float speed = 100;
+    [SerializeField] float speed; //0
+    [SerializeField] Transform upperLimit;
+    [SerializeField] Transform lowerLimit;
+    [SerializeField] Transform from;
+    [SerializeField] Transform to;
+    [SerializeField] GameObject sphere;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float direction = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime; // calculate distance to move
+        if (direction == -1 && transform.position.y > lowerLimit.position.y)
+        {
+            Debug.Log("Going down " + transform.position.y);
+            transform.position = Vector3.MoveTowards(transform.position, lowerLimit.position, step);
+            if (Mathf.Approximately(transform.position.y, lowerLimit.position.y))
+            {
+                direction = 0;
+                sphere.transform.parent = transform.transform;
+            }
+        }
+        else if (direction == 1 && transform.position.y < upperLimit.position.y)
+        {
+            Debug.Log("Going up " + transform.position.y);
+            transform.position = Vector3.MoveTowards(transform.position, upperLimit.position, step);
+            if (Mathf.Approximately(transform.position.y, upperLimit.position.y))
+            {
+                direction = 0;
+            }
+        }
+
+        /*
+        if (direction == -1 && transform.position.x > from.position.x)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, from.position, step);
+            if (Mathf.Approximately(transform.position.x, from.position.x))
+            {
+                direction = 0;
+            }
+            else if (direction == 1 && transform.position.x < to.position.x)
+            {
+
+            }
+        }
+        */
     }
 
-    public void MoveUp()
+    public void RedButtonPressed()
     {
-        transform.Translate(Vector3.up * Time.deltaTime);
+        direction = -1;
     }
-    public void MoveDown()
+    public void RedButtonReleased()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
-    }
-
-    public void MoveLeft()
-    {
-        transform.Translate(Vector3.left * Time.deltaTime);
+        //direction = 0;
     }
 
-    public void MoveRight()
+    public void BlueButtonPressed()
     {
-        transform.Translate(-Vector3.right * Time.deltaTime);
+        direction = 1;
+    }
+    public void BlueButtonReleased()
+    {
+        //direction = 0;
+    }
+
+    public void GreenButtonPressed()
+    {
+        direction = 1;
     }
 }
